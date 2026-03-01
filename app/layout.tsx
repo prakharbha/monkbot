ad372cfac40858e4fd8e07b65fb8a940ec55dff9
prakharbhatia <prakharb88@gmail.com>
2026-03-02 04:06:10 +0530

    feat: add GA4, sitemap, robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -10,9 +11,23 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-      </head>
-      <body>{children}</body>
+      <head />
+      <body>
+        {children}
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H8Q2G85TCC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H8Q2G85TCC');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
